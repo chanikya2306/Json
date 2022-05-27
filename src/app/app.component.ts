ff77c9716eb2b9bd742f0ad7e106131ba52244d2
chanikya2306 <chanikya2306@gmail.com>
2022-05-27 05:33:26 +0530

    Data Are removed from AppComponents And Changed into Main Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,} from '@angular/core';
 import data from '../assets/data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'topicjson';
-  display:any;
-  array:any=[]
-  showitems:any
-  constructor(){
-    this.display = data;
-    console.log(this.display)
-    this.array.push(this.display)
+
   }
-  ngOnInit(): void {
-  }
-}
+ 
